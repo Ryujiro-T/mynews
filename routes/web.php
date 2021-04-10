f682b344f62a73fd19d2a6dd3ce4f5321e085759
EC2 Default User <ec2-user@ip-172-31-5-188.us-east-2.compute.internal>
2021-04-10 01:20:10 +0000

    21 commit

--- routes/web.php
+++ routes/web.php
@@ -10,20 +10,33 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create',
+    'Admin\NewsController@add')->middleware('auth');
+     Route::get('profile/create',
+    'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit',
+    'Admin\ProfileController@edit')->middleware('auth');
+});
 
 //課題３
-Route::get('XXX',
-'Admin/AAAController@bbb');
+// Route::get('XXX',
+// 'Admin/AAAController@bbb');
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 //課題４
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/create',
-    'Admin/ProfileController@add');
-    Route::get('profile/edit',
-    'Admin/ProfileController@edit');
-});
 
+// Route::group(['prefix' => 'admin'], function() {
+//     Route::get('profile/create',
+//     'Admin/ProfileController@add');
+//     Route::get('profile/edit',
+//     'Admin/ProfileController@edit');
+// });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
