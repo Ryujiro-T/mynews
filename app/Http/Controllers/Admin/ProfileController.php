d3e9364e52d33255ee026ab0e549905a592100d4
EC2 Default User <ec2-user@ip-172-31-5-188.us-east-2.compute.internal>
2021-04-23 14:02:05 +0000

    23

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //以下を追記
@@ -12,12 +14,21 @@ class ProfileController extends Controller
     {
         return view('admin.profile.create');
     }
-    
-    public function create()
+    public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+        
+        $profile = new profile;
+        $form = $request->all();
+        
+        unset($form['_token']);
+        
+        $profile->fill($form);
+        $profile->save();
+        
         return redirect('admin/profile/create');
     }
-    
+
     public function edit()
     {
         return view('admin.profile.edit');
@@ -28,4 +39,3 @@ class ProfileController extends Controller
         return redirect('admin/profile/edit');
     }
 }
-
